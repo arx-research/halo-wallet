b54977516ce6db8e2defb382b1ee9909ecbf58a0
Leandro Barbosa <leandrobar93@gmail.com>
2024-01-02 12:18:55 -0300

    fix: problem with wallet connect

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,9 @@
-const WALLET_CONNECT_CLOUD_PROJECT_ID = 'b1b915089c9379d3a1d1bd001f8e6040';
+const WALLET_CONNECT_CLOUD_PROJECT_ID = "c541f47e9cf404783453630df3065207";
 const WALLET_CONNECT_APP_METADATA = {
-    name: 'Arx Wallet App',
-    description: 'HaLo Client for Wallet Connect',
-    url: 'arx.org',
-    icons: []
+  name: "Paradigma Wallet",
+  description: "Paradigma Client for Wallet Connect",
+  url: "paradigma.education",
+  icons: [],
 };
 
-export {WALLET_CONNECT_CLOUD_PROJECT_ID, WALLET_CONNECT_APP_METADATA};
-
+export { WALLET_CONNECT_CLOUD_PROJECT_ID, WALLET_CONNECT_APP_METADATA };
