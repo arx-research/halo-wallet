b7cbbc2c259dd78cc034a1219d3067efcdddaa00
Leandro Barbosa <leandrobar93@gmail.com>
2024-01-04 03:41:40 -0300

    refactor: alert

--- src/sub/SessionRequest.js
+++ src/sub/SessionRequest.js
@@ -1,13 +1,17 @@
-import {web3wallet} from "./WalletConnect";
-import {HaloWallet} from "../logic/HaloWallet.ts";
-import {formatJsonRpcError, formatJsonRpcResult} from '@json-rpc-tools/utils'
-import {EIP155_CHAINS, EIP155_MAINNET_CHAINS, EIP155_SIGNING_METHODS} from "../logic/EIP155Chains.js";
-import {ethers} from "ethers";
-import {arrayify} from "ethers/lib/utils";
-import JSONPretty from 'react-json-pretty';
+import { web3wallet } from "./WalletConnect";
+import { HaloWallet } from "../logic/HaloWallet.ts";
+import { formatJsonRpcError, formatJsonRpcResult } from "@json-rpc-tools/utils";
+import {
+  EIP155_CHAINS,
+  EIP155_MAINNET_CHAINS,
+  EIP155_SIGNING_METHODS,
+} from "../logic/EIP155Chains.js";
+import { ethers } from "ethers";
+import { arrayify } from "ethers/lib/utils";
+import JSONPretty from "react-json-pretty";
 import Button from "./Button.tsx";
 
-function SessionRequest({haloAddress, sessionRequest, onRequestProcessed}) {
+function SessionRequest({ haloAddress, sessionRequest, onRequestProcessed }) {
   async function processCall(req) {
     const { topic, params, id } = req;
     const { chainId, request } = params;
@@ -18,7 +22,9 @@ function SessionRequest({haloAddress, sessionRequest, onRequestProcessed}) {
     switch (request.method) {
       case EIP155_SIGNING_METHODS.PERSONAL_SIGN:
         try {
-          const signedMessage = await wallet.signMessage(arrayify(requestParamsMessage));
+          const signedMessage = await wallet.signMessage(
+            arrayify(requestParamsMessage),
+          );
           return formatJsonRpcResult(id, signedMessage);
         } catch (error) {
           return formatJsonRpcError(id, error.message);
@@ -26,7 +32,11 @@ function SessionRequest({haloAddress, sessionRequest, onRequestProcessed}) {
 
       case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V4:
         try {
-          const {domain, types, message: data} = JSON.parse(request.params[1]);
+          const {
+            domain,
+            types,
+            message: data,
+          } = JSON.parse(request.params[1]);
           delete types.EIP712Domain;
           const signedData = await wallet._signTypedData(domain, types, data);
           return formatJsonRpcResult(id, signedData);
@@ -39,8 +49,10 @@ function SessionRequest({haloAddress, sessionRequest, onRequestProcessed}) {
           console.log(chainId);
           console.log(EIP155_CHAINS[chainId]);
           console.log(EIP155_CHAINS[chainId].rpc);
-          const provider = new ethers.providers.JsonRpcProvider(EIP155_CHAINS[chainId].rpc);
-          console.log('provider', provider);
+          const provider = new ethers.providers.JsonRpcProvider(
+            EIP155_CHAINS[chainId].rpc,
+          );
+          console.log("provider", provider);
           const sendTransaction = request.params[0];
           const connectedWallet = wallet.connect(provider);
 
@@ -49,28 +61,33 @@ function SessionRequest({haloAddress, sessionRequest, onRequestProcessed}) {
             delete sendTransaction.gas;
           }
 
-          const { hash } = await connectedWallet.sendTransaction(sendTransaction);
+          const { hash } =
+            await connectedWallet.sendTransaction(sendTransaction);
           return formatJsonRpcResult(id, hash);
         } catch (error) {
-          console.error('sendTransaction error', error);
+          console.error("sendTransaction error", error);
           return formatJsonRpcError(id, error.message);
         }
 
       default:
-        throw new Error("Internal error, session request had an unsupported method: " + request.method);
+        throw new Error(
+          "Internal error, session request had an unsupported method: " +
+            request.method,
+        );
     }
   }
 
   async function btnSignTransaction() {
-    const { topic } = sessionRequest
+    const { topic } = sessionRequest;
     const response = await processCall(sessionRequest);
     await web3wallet.respondSessionRequest({ topic, response });
+    alert("Transaction signed", JSON.stringify(response));
     onRequestProcessed();
   }
 
   async function btnReject() {
-    const { topic, params, id } = sessionRequest
-    let response = formatJsonRpcError(id, 'Operation rejected by the user.');
+    const { topic, params, id } = sessionRequest;
+    let response = formatJsonRpcError(id, "Operation rejected by the user.");
     await web3wallet.respondSessionRequest({ topic, response });
     onRequestProcessed();
   }
@@ -94,33 +111,56 @@ function SessionRequest({haloAddress, sessionRequest, onRequestProcessed}) {
   switch (request.method) {
     case EIP155_SIGNING_METHODS.PERSONAL_SIGN:
       reqType = <span>sign personal message</span>;
-      vizComponent = <code style={{wordBreak: 'break-all'}}>{requestParamsMessage}</code>;
+      vizComponent = (
+        <code style={{ wordBreak: "break-all" }}>{requestParamsMessage}</code>
+      );
       break;
 
     case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V4:
       reqType = <span>sign typed data</span>;
-      vizComponent = <JSONPretty id="json-pretty" data={request.params[1]}></JSONPretty>;
+      vizComponent = (
+        <JSONPretty id="json-pretty" data={request.params[1]}></JSONPretty>
+      );
       break;
 
     case EIP155_SIGNING_METHODS.ETH_SEND_TRANSACTION:
       reqType = <span>sign and send transaction</span>;
-      vizComponent = <JSONPretty id="json-pretty" data={request.params[0]}></JSONPretty>;
+      vizComponent = (
+        <JSONPretty id="json-pretty" data={request.params[0]}></JSONPretty>
+      );
       break;
   }
 
   return (
-      <div>
-        <p className={"label-text"}>
-          Request from the dApp:<br /><span style={{color: 'white'}}>{peerMeta.name} ({chainName})</span>
-          <br />to:<br /><span style={{color: 'white'}}>{reqType}</span>:
-        </p>
-        <div style={{fontSize: 10, height: '300px', overflow: 'scroll', margin: '20px 0'}}>{vizComponent}</div>
-        <p className={"label-text"} style={{margin: '20px 0'}}>
-          You will need to scan your HaLo after clicking the "Sign request" button.
-        </p>
-        <Button onClick={() => btnSignTransaction()}>Sign request</Button>
-        <Button onClick={() => btnReject()}>Deny</Button>
+    <div>
+      <p className={"label-text"}>
+        Request from the dApp:
+        <br />
+        <span style={{ color: "white" }}>
+          {peerMeta.name} ({chainName})
+        </span>
+        <br />
+        to:
+        <br />
+        <span style={{ color: "white" }}>{reqType}</span>:
+      </p>
+      <div
+        style={{
+          fontSize: 10,
+          height: "300px",
+          overflow: "scroll",
+          margin: "20px 0",
+        }}
+      >
+        {vizComponent}
       </div>
+      <p className={"label-text"} style={{ margin: "20px 0" }}>
+        You will need to scan your HaLo after clicking the "Sign request"
+        button.
+      </p>
+      <Button onClick={() => btnSignTransaction()}>Sign request</Button>
+      <Button onClick={() => btnReject()}>Deny</Button>
+    </div>
   );
 }
 
