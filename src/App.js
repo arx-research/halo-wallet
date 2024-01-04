8827b257750609918e6ff15ea3fad02809fab6e5
Leandro Barbosa <leandrobar93@gmail.com>
2024-01-04 00:21:00 -0300

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './style.scss';
-import 'react-json-pretty/themes/monikai.css';
+import "./style.scss";
+import "react-json-pretty/themes/monikai.css";
 
-import {useCallback, useEffect, useState} from "react";
+import { useCallback, useEffect, useState } from "react";
 import ScanWeb3ModalQR from "./sub/ScanWeb3ModalQR";
-import {wcCreate, wcGetPairings, wcPair, web3wallet} from "./sub/WalletConnect";
+import {
+  wcCreate,
+  wcGetPairings,
+  wcPair,
+  web3wallet,
+} from "./sub/WalletConnect";
 import SessionProposal from "./sub/SessionProposal";
 import SessionRequest from "./sub/SessionRequest";
 import Header from "./sub/Header.tsx";
@@ -11,20 +16,20 @@ import Wrapper from "./sub/Wrapper.tsx";
 import ListPairings from "./sub/ListPairings";
 
 function App() {
-  let [appState, setAppState] = useState({name: "init"});
+  let [appState, setAppState] = useState({ name: "init" });
   let [haloAddress, setHaloAddress] = useState(null);
 
-  console.log('appState', appState);
+  console.log("appState", appState);
 
   function updateHaloAddress(addr) {
-    window.localStorage.setItem('haloAddress', addr);
+    window.localStorage.setItem("haloAddress", addr);
     setHaloAddress(addr);
   }
 
   function postResetWallet() {
-    window.localStorage.removeItem('haloAddress');
+    window.localStorage.removeItem("haloAddress");
     setHaloAddress(null);
-    setAppState({"name": "pairing"});
+    setAppState({ name: "pairing" });
   }
 
   function switchToMainScreen() {
@@ -33,46 +38,46 @@ function App() {
     let hasAnyPairings = Object.keys(wcGetPairings()).length > 0;
 
     if (pendingProposals.length > 0) {
-      setAppState({"name": "session_proposal", "proposal": pendingProposals[0]});
+      setAppState({ name: "session_proposal", proposal: pendingProposals[0] });
     } else if (pendingReqs.length > 0) {
-      setAppState({"name": "session_request", "request": pendingReqs[0]});
+      setAppState({ name: "session_request", request: pendingReqs[0] });
     } else if (hasAnyPairings) {
-      setAppState({"name": "paired"});
+      setAppState({ name: "paired" });
     } else {
-      setAppState({"name": "pairing"});
+      setAppState({ name: "pairing" });
     }
   }
 
   const onSessionProposal = useCallback((proposal) => {
     setAppState({
       name: "session_proposal",
-      proposal
+      proposal,
     });
   }, []);
 
   const onSessionRequest = useCallback((request) => {
-    console.log('session_request', request);
+    console.log("session_request", request);
 
-    setAppState(prevAppState => {
+    setAppState((prevAppState) => {
       if (prevAppState.name === "session_request") {
-        console.log('session_request already pending');
+        console.log("session_request already pending");
         return prevAppState;
       } else {
-        return {name: "session_request", request};
+        return { name: "session_request", request };
       }
     });
   }, []);
 
   const onSessionDelete = useCallback((request) => {
-    console.log('session_delete', request);
+    console.log("session_delete", request);
     let hasAnyPairings = Object.keys(wcGetPairings()).length > 0;
 
-    setAppState(prevAppState => {
+    setAppState((prevAppState) => {
       if (!hasAnyPairings) {
-        return {"name": "pairing"};
+        return { name: "pairing" };
       } else if (prevAppState.name === "paired") {
         // force re-render
-        return {"name": "paired"};
+        return { name: "paired" };
       }
     });
   }, []);
@@ -83,7 +88,7 @@ function App() {
         await wcCreate({
           onSessionProposal,
           onSessionRequest,
-          onSessionDelete
+          onSessionDelete,
         });
       } catch (e) {
         alert(e.toString());
@@ -93,7 +98,7 @@ function App() {
       switchToMainScreen();
     }
 
-    let addr = window.localStorage.getItem('haloAddress');
+    let addr = window.localStorage.getItem("haloAddress");
 
     if (addr) {
       setHaloAddress(addr);
@@ -106,11 +111,11 @@ function App() {
     async function pairWalletConnect() {
       try {
         await wcPair({
-          uri: appState.pairURI
+          uri: appState.pairURI,
         });
       } catch (e) {
         alert(e.toString());
-        setAppState({name: "pairing"});
+        setAppState({ name: "pairing" });
       }
     }
 
@@ -124,22 +129,27 @@ function App() {
       case "init":
         return <p>Initializing...</p>;
       case "pairing":
-        return <ScanWeb3ModalQR
-            onScan={(pairURI) => setAppState({name: "do_pair", pairURI})}
+        return (
+          <ScanWeb3ModalQR
+            onScan={(pairURI) => setAppState({ name: "do_pair", pairURI })}
             onError={(err) => alert(err)}
-            onCancel={() => setAppState({"name": "paired"})}
-        />;
+            onCancel={() => setAppState({ name: "paired" })}
+          />
+        );
       case "paired":
-        return <ListPairings
+        return (
+          <ListPairings
             haloAddress={haloAddress}
-            onStartPairing={() => setAppState({"name": "pairing"})}
+            onStartPairing={() => setAppState({ name: "pairing" })}
             onSwitchHalo={(addr) => updateHaloAddress(addr)}
             onResetWallet={() => postResetWallet()}
-        />;
+          />
+        );
       case "do_pair":
         return <p className={"label-text"}>Pairing...</p>;
       case "session_proposal":
-        return <SessionProposal
+        return (
+          <SessionProposal
             proposal={appState.proposal}
             haloAddress={haloAddress}
             onProposalProcessed={(addr) => {
@@ -149,23 +159,26 @@ function App() {
             onProposalDenied={() => {
               switchToMainScreen();
             }}
-        />;
+          />
+        );
       case "session_request":
-        return <SessionRequest
+        return (
+          <SessionRequest
             sessionRequest={appState.request}
             haloAddress={haloAddress}
             onRequestProcessed={() => switchToMainScreen()}
-        />;
+          />
+        );
       default:
         return <p className={"label-text"}>Unknown app state.</p>;
     }
   }
 
   return (
-      <Wrapper>
-        <Header />
-        {getMainComponent()}
-      </Wrapper>
+    <Wrapper>
+      <Header />
+      {getMainComponent()}
+    </Wrapper>
   );
 }
 
